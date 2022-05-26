15466512f8bcec624807af09becc9f34cddea7ec
Nihisil <alexey@nihisil.com>
2022-05-26 17:44:33 +0700

    [#13] Store HTML page content in background job

---
+++ test/google_scraping/keyword_scraper_test.exs
@@ -0,0 +1,14 @@
+defmodule GoogleScraping.Dashboard.Schemas.KeywordScraperTest do
+  use GoogleScraping.DataCase, async: true
+
+  alias GoogleScraping.Dashboard.KeywordScraper
+
+  describe "get_search_page_html_for_keyword/1" do
+    test "given keyword, returns HTML search result" do
+      use_cassette "google/cat" do
+        {:ok, response} = KeywordScraper.get_search_page_html_for_keyword("cat")
+        assert response != nil
+      end
+    end
+  end
+end
---
+++ test/google_scraping/keyword_worker_test.exs
@@ -0,0 +1,31 @@
+defmodule GoogleScraping.Dashboard.Schemas.KeywordScraperWorkerTest do
+  use GoogleScraping.DataCase, async: true
+
+  alias GoogleScraping.Dashboard
+  alias GoogleScraping.Dashboard.KeywordScraperWorker
+
+  describe "perform/1" do
+    test "given keyword, download and store HTML" do
+      use_cassette "google/cat" do
+        keyword = insert(:keyword)
+
+        Oban.insert(KeywordScraperWorker.new(%{keyword_id: keyword.id}))
+
+        updated_keyword = Dashboard.get_keyword_by_id!(keyword.id)
+        assert updated_keyword.status == :completed
+        assert updated_keyword.html != nil
+      end
+    end
+
+    test "given error for keyword search, fails job" do
+      stub(HTTPoison, :get, fn _, _ -> {:error, %{reason: :error}} end)
+
+      keyword = insert(:keyword)
+
+      Oban.insert(KeywordScraperWorker.new(%{keyword_id: keyword.id}))
+
+      updated_keyword = Dashboard.get_keyword_by_id!(keyword.id)
+      assert updated_keyword.status == :failed
+    end
+  end
+end
