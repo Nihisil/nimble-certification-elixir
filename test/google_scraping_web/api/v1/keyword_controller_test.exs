297aeb60581c41132700a4decc10ae4c2e7d959d
Nihisil <alexey@nihisil.com>
2022-08-10 16:29:48 +0700

    [#32] Add search by keyword name API

--- test/google_scraping_web/api/v1/keyword_controller_test.exs
+++ test/google_scraping_web/api/v1/keyword_controller_test.exs
@@ -29,5 +29,33 @@ defmodule GoogleScrapingWeb.Api.V1.KeywordControllerTest do
                "included" => []
              } = json_response(conn, 200)
     end
+
+    test "with provided search phrase, returns filtered queryset", %{conn: conn} do
+      user = insert(:user)
+      _cat_keyword = insert(:keyword, name: "cat", user_id: user.id)
+      _dog_keyword = insert(:keyword, name: "dog", user_id: user.id)
+
+      conn =
+        conn
+        |> token_auth_user(user)
+        |> get(Routes.api_keyword_path(conn, :index, %{"query" => "og"}))
+
+      assert %{
+               "data" => [
+                 %{
+                   "attributes" => %{
+                     "name" => "dog",
+                     "status" => _,
+                     "updated_at" => _,
+                     "inserted_at" => _
+                   },
+                   "id" => _,
+                   "relationships" => %{},
+                   "type" => "keywords"
+                 }
+               ],
+               "included" => []
+             } = json_response(conn, 200)
+    end
   end
 end
