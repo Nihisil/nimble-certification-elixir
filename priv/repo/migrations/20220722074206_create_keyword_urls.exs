cfccb6db7caf78005e9619fec82f658cd9bdd639
Nihisil <alexey@nihisil.com>
2022-08-23 16:46:11 +0700

    [#23] Use not null value for boolean field

--- priv/repo/migrations/20220722074206_create_keyword_urls.exs
+++ priv/repo/migrations/20220722074206_create_keyword_urls.exs
@@ -4,7 +4,7 @@ defmodule GoogleScraping.Repo.Migrations.CreateKeywordUrls do
   def change do
     create table(:keyword_urls) do
       add :url, :text, null: false
-      add :is_ad, :boolean
+      add :is_ad, :boolean, null: false
 
       add :keyword_id, references(:keywords, on_delete: :delete_all), null: false
       add :user_id, references(:users, on_delete: :delete_all), null: false
